db38ee39f29aa053d3dcfece17e6f2ca1ed9b462
dkomatovic <danicakomatovic@gmail.com>
2026-04-16 13:06:15 +0200

    Created models and enums

---
+++ ProcessingSystem/Models/Job.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProcessingSystem.Enums;
+
+namespace ProcessingSystem.Models
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+        public JobType Type { get; set; }
+        public string Payload { get; set; }
+        public int Priority { get; set; }
+
+        public Job(JobType type, string payload, int priority)
+        {
+            Id = Guid.NewGuid();
+            Type = type;
+            Payload = payload;
+            if (priority > 0)
+            {
+                Priority = priority;
+            } else
+            {
+                throw new InvalidDataException("Priority cannot be equal to or less than 0");
+            }
+        }
+    }
+}
---
+++ ProcessingSystem/Models/JobHandle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessingSystem.Models
+{
+    public class JobHandle
+    {
+        public Guid Id { get; set; }
+        public Task<int> Result { get; set; }
+
+        public JobHandle(Guid id, Task<int> result)
+        {
+            Id = id;
+            Result = result;
+        }
+    }
+}
